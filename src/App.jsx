d5ab74ce9c7a70c10e7b43812a95f56348519bd7
Damjan15 <stamenovdamjan5@gmail.com>
2024-01-20 14:12:41 +0100

    feat: created state which allows us to switch between creating project and no project components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
+import { useState } from "react";
 import Project from "./components/Project";
 import Sidebar from "./components/Sidebar";
 import NoProject from "./components/NoProject";
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: []
+  });
+  let content;
+
+  const handleStartAddProject = () => {
+    setProjectsState((prevState) => ({
+      ...prevState,
+      selectedProjectId: null
+    }));
+  }
+
+  if (projectsState.selectedProjectId === null) {
+    content = <Project />
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProject onStartAddProject={handleStartAddProject} />
+  }
+
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <Sidebar />
-      <NoProject />
+      <Sidebar onStartAddProject={handleStartAddProject} />
+      { content }
     </main>
   );
 }
--- src/components/NoProject.jsx
+++ src/components/NoProject.jsx
@@ -1,7 +1,7 @@
 import noProjectImage from "../assets/no-projects.png"
 import Button from "./Button"
 
-const NoProject = () => {
+const NoProject = ({ onStartAddProject }) => {
   return (
     <div className="mt-24 text-center w-2/3">
       <img src={noProjectImage} alt="no-project" className="w-16 h-16 object-contain mx-auto" />
@@ -9,7 +9,7 @@ const NoProject = () => {
       <h2 className="text-xl font-bold text-stone-500 my-4">No Project Selected</h2>
       <p className="text-stone-400 mb-4">Select a project or get started with a new one</p>
       <p className="mt-8">
-        <Button>Create new project</Button>
+        <Button onClick={onStartAddProject}>Create new project</Button>
       </p>
     </div>
   )
