7d2985de1e865f815459b741975eb8ae52f24c9b
Damjan15 <stamenovdamjan5@gmail.com>
2024-01-20 13:52:43 +0100

    feat: created Project component, created Input component & refactored components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Project from "./components/Project";
 import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
-    <main className="h-screen my-8">
+    <main className="h-screen my-8 flex gap-8">
       <Sidebar />
+      <Project />
     </main>
   );
 }
