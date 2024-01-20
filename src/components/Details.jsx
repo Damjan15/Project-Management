55dd7a9d7a811f41802c77ed08a329a996e90efe
Damjan15 <stamenovdamjan5@gmail.com>
2024-01-20 23:58:34 +0100

    feat: added Tasks & NewTask component

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,3 +1,5 @@
+import Tasks from "./Tasks";
+
 const Details = ({ project, onDelete }) => {
     const formattedDate = new Date(project.dueDate).toLocaleDateString('en-US', {
         year: 'numeric',
@@ -16,6 +18,8 @@ const Details = ({ project, onDelete }) => {
         <p className="mb-4 text-stone-400">{formattedDate}</p>
         <p className="text-stone-600 whitespace-pre-wrap">{project.description}</p>
       </header>
+
+      <Tasks />
     </div>
   );
 };
